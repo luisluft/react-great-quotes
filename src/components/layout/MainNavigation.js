4c35bea10f97a7c2e4a7422637472e84468e50ce
luisluft <luberluft@gmail.com>
2021-12-06 09:08:00 -0400

    add dummy quotes

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -15,7 +15,7 @@ const MainNavigation = () => {
           </li>
           <li>
             <NavLink to="/new-quote" activeClassName={classes.active}>
-              New Quote
+              Add a Quote
             </NavLink>
           </li>
         </ul>
--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,13 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "React is fun" },
+  { id: "q2", author: "Luis", text: "React is not so much fun" },
+];
 
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
